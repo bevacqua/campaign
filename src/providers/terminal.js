3cdf09a1a33f95e250644ebf92dc8e23c914b5f6
Nicolas Bevacqua <nicolasbevacqua@gmail.com>
2014-10-23 23:01:40 -0300

    bump campaign

--- src/providers/terminal.js
+++ src/providers/terminal.js
@@ -3,9 +3,6 @@
 var ultramarked = require('ultramarked');
 var htmlmd = require('html-md');
 var options = {
-  smartLists: true,
-  ultralight: true,
-  ultrasanitize: true,
   terminal: true
 };
 
@@ -24,7 +21,7 @@ module.exports = function () {
             var md = htmlmd(body);
             var term = ultramarked(md, options);
 
-            console.log(JSON.stringify(model, null, 2));
+            // console.log(JSON.stringify(model, null, 2));
             console.log(term);
             done();
         }
